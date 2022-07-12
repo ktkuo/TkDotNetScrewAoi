978f7fe680fb95efc72a072a046b1756015f8107
ktkuo <mon61188@gmail.com>
2022-07-12 17:20:48 +0800

    加入專案檔案。

---
+++ TkDotNetScrewAoi/view/FormImagGet.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace TkDotNetScrewAoi.view
+{
+    partial class FormImagGet
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.hWControl_Roi1 = new HalconDotNet.HWindowControl();
+            this.hWindowControl2 = new HalconDotNet.HWindowControl();
+            this.hWindowControl3 = new HalconDotNet.HWindowControl();
+            this.hWindowControl4 = new HalconDotNet.HWindowControl();
+            this.hWindowControl1 = new HalconDotNet.HWindowControl();
+            this.hWindowControl5 = new HalconDotNet.HWindowControl();
+            this.hWindowControl6 = new HalconDotNet.HWindowControl();
+            this.hWindowControl7 = new HalconDotNet.HWindowControl();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.hWindowControl7);
+            this.panel1.Controls.Add(this.hWindowControl6);
+            this.panel1.Controls.Add(this.hWindowControl5);
+            this.panel1.Controls.Add(this.hWindowControl1);
+            this.panel1.Controls.Add(this.hWindowControl4);
+            this.panel1.Controls.Add(this.hWindowControl3);
+            this.panel1.Controls.Add(this.hWindowControl2);
+            this.panel1.Controls.Add(this.hWControl_Roi1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1257, 521);
+            this.panel1.TabIndex = 0;
+            // 
+            // hWControl_Roi1
+            // 
+            this.hWControl_Roi1.BackColor = System.Drawing.Color.Black;
+            this.hWControl_Roi1.BorderColor = System.Drawing.Color.Black;
+            this.hWControl_Roi1.ImagePart = new System.Drawing.Rectangle(0, 0, 2448, 2048);
+            this.hWControl_Roi1.Location = new System.Drawing.Point(3, 3);
+            this.hWControl_Roi1.Name = "hWControl_Roi1";
+            this.hWControl_Roi1.Size = new System.Drawing.Size(306, 256);
+            this.hWControl_Roi1.TabIndex = 0;
+            this.hWControl_Roi1.WindowSize = new System.Drawing.Size(306, 256);
+            // 
+            // hWindowControl2
+            // 
+            this.hWindowControl2.BackColor = System.Drawing.Color.Black;
+            this.hWindowControl2.BorderColor = System.Drawing.Color.Black;
+            this.hWindowControl2.ImagePart = new System.Drawing.Rectangle(0, 0, 2448, 2048);
+            this.hWindowControl2.Location = new System.Drawing.Point(315, 3);
+            this.hWindowControl2.Name = "hWindowControl2";
+            this.hWindowControl2.Size = new System.Drawing.Size(306, 256);
+            this.hWindowControl2.TabIndex = 1;
+            this.hWindowControl2.WindowSize = new System.Drawing.Size(306, 256);
+            // 
+            // hWindowControl3
+            // 
+            this.hWindowControl3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.hWindowControl3.BackColor = System.Drawing.Color.Black;
+            this.hWindowControl3.BorderColor = System.Drawing.Color.Black;
+            this.hWindowControl3.ImagePart = new System.Drawing.Rectangle(0, 0, 2448, 2048);
+            this.hWindowControl3.Location = new System.Drawing.Point(636, 3);
+            this.hWindowControl3.Name = "hWindowControl3";
+            this.hWindowControl3.Size = new System.Drawing.Size(306, 256);
+            this.hWindowControl3.TabIndex = 2;
+            this.hWindowControl3.WindowSize = new System.Drawing.Size(306, 256);
+            // 
+            // hWindowControl4
+            // 
+            this.hWindowControl4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.hWindowControl4.BackColor = System.Drawing.Color.Black;
+            this.hWindowControl4.BorderColor = System.Drawing.Color.Black;
+            this.hWindowControl4.ImagePart = new System.Drawing.Rectangle(0, 0, 2448, 2048);
+            this.hWindowControl4.Location = new System.Drawing.Point(948, 3);
+            this.hWindowControl4.Name = "hWindowControl4";
+            this.hWindowControl4.Size = new System.Drawing.Size(306, 256);
+            this.hWindowControl4.TabIndex = 3;
+            this.hWindowControl4.WindowSize = new System.Drawing.Size(306, 256);
+            // 
+            // hWindowControl1
+            // 
+            this.hWindowControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.hWindowControl1.BackColor = System.Drawing.Color.Black;
+            this.hWindowControl1.BorderColor = System.Drawing.Color.Black;
+            this.hWindowControl1.ImagePart = new System.Drawing.Rectangle(0, 0, 2448, 2048);
+            this.hWindowControl1.Location = new System.Drawing.Point(3, 262);
+            this.hWindowControl1.Name = "hWindowControl1";
+            this.hWindowControl1.Size = new System.Drawing.Size(306, 256);
+            this.hWindowControl1.TabIndex = 4;
+            this.hWindowControl1.WindowSize = new System.Drawing.Size(306, 256);
+            // 
+            // hWindowControl5
+            // 
+            this.hWindowControl5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.hWindowControl5.BackColor = System.Drawing.Color.Black;
+            this.hWindowControl5.BorderColor = System.Drawing.Color.Black;
+            this.hWindowControl5.ImagePart = new System.Drawing.Rectangle(0, 0, 2448, 2048);
+            this.hWindowControl5.Location = new System.Drawing.Point(315, 262);
+            this.hWindowControl5.Name = "hWindowControl5";
+            this.hWindowControl5.Size = new System.Drawing.Size(306, 256);
+            this.hWindowControl5.TabIndex = 5;
+            this.hWindowControl5.WindowSize = new System.Drawing.Size(306, 256);
+            // 
+            // hWindowControl6
+            // 
+            this.hWindowControl6.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.hWindowControl6.BackColor = System.Drawing.Color.Black;
+            this.hWindowControl6.BorderColor = System.Drawing.Color.Black;
+            this.hWindowControl6.ImagePart = new System.Drawing.Rectangle(0, 0, 2448, 2048);
+            this.hWindowControl6.Location = new System.Drawing.Point(636, 262);
+            this.hWindowControl6.Name = "hWindowControl6";
+            this.hWindowControl6.Size = new System.Drawing.Size(306, 256);
+            this.hWindowControl6.TabIndex = 6;
+            this.hWindowControl6.WindowSize = new System.Drawing.Size(306, 256);
+            // 
+            // hWindowControl7
+            // 
+            this.hWindowControl7.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.hWindowControl7.BackColor = System.Drawing.Color.Black;
+            this.hWindowControl7.BorderColor = System.Drawing.Color.Black;
+            this.hWindowControl7.ImagePart = new System.Drawing.Rectangle(0, 0, 2448, 2048);
+            this.hWindowControl7.Location = new System.Drawing.Point(948, 262);
+            this.hWindowControl7.Name = "hWindowControl7";
+            this.hWindowControl7.Size = new System.Drawing.Size(306, 256);
+            this.hWindowControl7.TabIndex = 7;
+            this.hWindowControl7.WindowSize = new System.Drawing.Size(306, 256);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Location = new System.Drawing.Point(12, 536);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1257, 181);
+            this.panel2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(3, 139);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(306, 39);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(3, 94);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(306, 39);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // FormImagGet
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(1281, 729);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Name = "FormImagGet";
+            this.Text = "FormImagGet";
+            this.Load += new System.EventHandler(this.FormImagGet_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private HalconDotNet.HWindowControl hWindowControl4;
+        private HalconDotNet.HWindowControl hWindowControl3;
+        private HalconDotNet.HWindowControl hWindowControl2;
+        private HalconDotNet.HWindowControl hWControl_Roi1;
+        private HalconDotNet.HWindowControl hWindowControl7;
+        private HalconDotNet.HWindowControl hWindowControl6;
+        private HalconDotNet.HWindowControl hWindowControl5;
+        private HalconDotNet.HWindowControl hWindowControl1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ TkDotNetScrewAoi/view/FormImagGet.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TkDotNetScrewAoi.view
+{
+    public partial class FormImagGet : Form
+    {
+        public FormImagGet()
+        {
+            InitializeComponent();
+        }
+
+        private void FormImagGet_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
